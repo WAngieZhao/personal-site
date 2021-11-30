c4e594ae7b58a8f92358ec80d3a97b3fc23329b8
Angie Zhao <wz1342@nyu.edu>
2021-11-30 12:06:28 -0500

    Inpage Resume

--- src/components/Resume/References.js
+++ src/components/Resume/References.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const References = () => (
   <div className="references">
     <div className="link-to" id="references" />
     <div className="title">
-      <Link to="/contact">
-        <h3><a href="https://drive.google.com/file/d/1twlCLVKxqs22ZVL2yhfzA3bkcuVWaB7M/view?usp=sharing"> More Detailed Downloadable Resume Can Be Accessed Here</a></h3>
-      </Link>
-      <iframe
-        src="https://docs.google.com/gview?url=https://drive.google.com/uc?id=1twlCLVKxqs22ZVL2yhfzA3bkcuVWaB7M&embedded=true"
-        frameBorder="0"
-        scrolling="auto"
-        height="400px"
-        width="100%"
-        title="pdf"
-      />
+      <h3><a href="https://drive.google.com/file/d/1twlCLVKxqs22ZVL2yhfzA3bkcuVWaB7M/view?usp=sharing"> More Detailed Downloadable Resume Can Be Accessed Here</a></h3>
+      <iframe src="https://drive.google.com/file/d/1twlCLVKxqs22ZVL2yhfzA3bkcuVWaB7M/preview" width="640" height="480" allow="autoplay" title="pdf" />
     </div>
   </div>
 );
